1e5de0cc218d89af0515c3612ed147307b137e4c
Alessio Berti <alessioberti90@gmail.com>
2023-05-04 09:59:10 +0200

    Change search of non abstract classes.

--- eventsource_subclasses.py
+++ eventsource_subclasses.py
@@ -1,5 +1,4 @@
 from ctapipe.io import EventSource
-from ctapipe.core import non_abstract_children
 
-for cls in non_abstract_children(EventSource):
+for cls in EventSource.non_abstract_subclasses().values():
     print(cls.__name__)
