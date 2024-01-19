56c1b8267a6ac7ef6a22c2eefec0febe613d770e
Alessio Berti <alessioberti90@gmail.com>
2024-01-19 10:52:37 +0100

    Remainder of previous commit.

--- ctapipe_io_magic/__init__.py
+++ ctapipe_io_magic/__init__.py
@@ -100,33 +100,6 @@ def load_camera_geometry():
     return CameraGeometry.from_table(f)
 
 
-class MissingInputFilesError(Exception):
-    """
-    Exception raised when the files check fails.
-    """
-
-    def __init__(self, message):
-        self.message = message
-
-
-class FailedFileCheckError(Exception):
-    """
-    Exception raised when the files check fails.
-    """
-
-    def __init__(self, message):
-        self.message = message
-
-
-class MissingDriveReportError(Exception):
-    """
-    Exception raised when a subrun does not have drive reports.
-    """
-
-    def __init__(self, message):
-        self.message = message
-
-
 class MAGICEventSource(EventSource):
     """
     EventSource for MAGIC calibrated data.
